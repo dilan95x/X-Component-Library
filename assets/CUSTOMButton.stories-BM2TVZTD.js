cae48a4212423aedbcdb6bd4532fdc0ac6836ebc
dilan95x <dilan95x@users.noreply.github.com>
2024-06-10 18:30:02 +0000

    deploy: b58de311899f6f253aac05cab661ed8552f65305

---
+++ assets/CUSTOMButton.stories-BM2TVZTD.js
@@ -0,0 +1,53 @@
+import{f as b}from"./index.es-BdLiqkeD.js";import{I as y}from"./index-DdwOGWFU.js";import"./index-CDs2tPxN.js";import"./index-27BIJqHF.js";const k={component:y,args:{onClick:()=>{alert("Button Clicked")}}},e={args:{children:"Button",color:"#0163CC",textcolor:"white",size:"lg",btnstyle:"contained",fontSize:"1rem"}},t={args:{children:"Button",color:"#0163CC",textcolor:"white",size:"lg",btnstyle:"outlined",fontSize:"1rem"}},o={args:{children:"Button",textcolor:"white",size:"lg",loading:!0,fontSize:"1rem"}},n={args:{children:"Button",textcolor:"white",size:"lg",disabled:!0,fontSize:"1rem"}},r={args:{children:"Button",startIcon:b,textcolor:"white",color:"#0163CC",size:"lg",fontSize:"1rem"}},s={args:{children:"Button",endIcon:b,textcolor:"white",color:"#0163CC",size:"lg",fontSize:"1rem"}};var a,c,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    color: "#0163CC",
+    textcolor: "white",
+    size: "lg",
+    btnstyle: "contained",
+    fontSize: "1rem"
+  }
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var l,d,u;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    color: "#0163CC",
+    textcolor: "white",
+    size: "lg",
+    btnstyle: "outlined",
+    fontSize: "1rem"
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var m,g,p;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    textcolor: "white",
+    size: "lg",
+    loading: true,
+    fontSize: "1rem"
+  }
+}`,...(p=(g=o.parameters)==null?void 0:g.docs)==null?void 0:p.source}}};var h,B,z;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    textcolor: "white",
+    size: "lg",
+    disabled: true,
+    fontSize: "1rem"
+  }
+}`,...(z=(B=n.parameters)==null?void 0:B.docs)==null?void 0:z.source}}};var C,S,f;r.parameters={...r.parameters,docs:{...(C=r.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    startIcon: faUser,
+    textcolor: "white",
+    color: "#0163CC",
+    size: "lg",
+    fontSize: "1rem"
+  }
+}`,...(f=(S=r.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var x,w,I;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    endIcon: faUser,
+    textcolor: "white",
+    color: "#0163CC",
+    size: "lg",
+    fontSize: "1rem"
+  }
+}`,...(I=(w=s.parameters)==null?void 0:w.docs)==null?void 0:I.source}}};const D=["ContainedButton","OutlinedButton","LoadingButton","DisabledButton","ButtonWithStartIcon","ButtonWithEndIcon"];export{s as ButtonWithEndIcon,r as ButtonWithStartIcon,e as ContainedButton,n as DisabledButton,o as LoadingButton,t as OutlinedButton,D as __namedExportsOrder,k as default};
---
+++ assets/CUSTOMFormField.stories-C7WpRhU6.js
@@ -0,0 +1,52 @@
+import{f as d}from"./index.es-BdLiqkeD.js";import{I as x}from"./index-DtLe4lku.js";import"./index-CDs2tPxN.js";import"./index-27BIJqHF.js";const k={component:x,args:{onChange:()=>{alert("Value Changed")}}},e=U=>{console.log(`Value Changed: ${U.target.value}`)},a={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e,stylevariant:"simple"}},r={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e,disabled:!0,stylevariant:"simple"}},n={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e,startIcon:d,stylevariant:"simple"}},s={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e,endIcon:d,stylevariant:"simple"}},t={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e}},o={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e,startIcon:d}},l={args:{label:"Custom Label",fieldwidth:"15rem",onChange:e,endIcon:d}};var i,m,c;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange,
+    stylevariant: "simple"
+  }
+}`,...(c=(m=a.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var h,u,g;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange,
+    disabled: true,
+    stylevariant: "simple"
+  }
+}`,...(g=(u=r.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var p,C,b;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange,
+    startIcon: faUser,
+    stylevariant: "simple"
+  }
+}`,...(b=(C=n.parameters)==null?void 0:C.docs)==null?void 0:b.source}}};var F,f,I;s.parameters={...s.parameters,docs:{...(F=s.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange,
+    endIcon: faUser,
+    stylevariant: "simple"
+  }
+}`,...(I=(f=s.parameters)==null?void 0:f.docs)==null?void 0:I.source}}};var w,L,S;t.parameters={...t.parameters,docs:{...(w=t.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange
+  }
+}`,...(S=(L=t.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var v,O,y;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange,
+    startIcon: faUser
+  }
+}`,...(y=(O=o.parameters)==null?void 0:O.docs)==null?void 0:y.source}}};var W,D,E;l.parameters={...l.parameters,docs:{...(W=l.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    label: "Custom Label",
+    fieldwidth: "15rem",
+    onChange: handleChange,
+    endIcon: faUser
+  }
+}`,...(E=(D=l.parameters)==null?void 0:D.docs)==null?void 0:E.source}}};const q=["OutlinedFormField","OutlinedFormFieldDisabled","OutlinedFormFieldWithStartIcon","OutlinedFormFieldWithEndIcon","DefaultFormField","DefaultFormFieldWithStartIcon","FormFieldWithEndIcon"];export{t as DefaultFormField,o as DefaultFormFieldWithStartIcon,l as FormFieldWithEndIcon,a as OutlinedFormField,r as OutlinedFormFieldDisabled,s as OutlinedFormFieldWithEndIcon,n as OutlinedFormFieldWithStartIcon,q as __namedExportsOrder,k as default};
