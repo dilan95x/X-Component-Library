ba88ec7e858cd3a71a63659712efe4af3d4fef27
Dilan <101853864+dilan95x@users.noreply.github.com>
2024-06-11 12:09:54 +0530

    new method

--- extract_version.sh
+++ extract_version.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Extract version from package.json
 version=$(node -p -e "require('./package.json').version")
 
-# Set the version as an output
-echo "::set-output name=version::$version"
+echo "VERSION=$version" > version.env
